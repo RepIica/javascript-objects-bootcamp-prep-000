1a327d59da55645268a57e7af24ba85dc4ee43a0
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 21:20:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-// var playlist = {Deadmau5: "Strobe"}
+var playlist = {Deadmau5: "Strobe"}
 
-// function updatePlaylist(object, artist, title) {
-//   return Object.assign({}, object, {[artist]: title})
-// }
+function updatePlaylist(object, artist, title) {
+  return Object.assign({}, object, {[artist]: title})
+}
 
-// function removeFromPlaylist(object, artist) {
-//   var newObj = Object.assign({}, object)
-//   delete newObj.artist
-//   return newObj
-// }
+function removeFromPlaylist(object, artist) {
+  var newObj = Object.assign({}, object)
+  delete newObj.artist
+  return newObj
+}
