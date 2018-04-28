50e39c3bc256895dcd27650867dc2d9479e5048c
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 21:26:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Deadmau5: "Strobe"}
+var playlist = { Deadmau5: "Strobe" }
 
 function updatePlaylist(object, artist, title) {
   return Object.assign({}, object, { [artist]: title } )
