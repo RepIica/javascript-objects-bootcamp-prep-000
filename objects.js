90f87120b1ec9fb2c41fdee5e019862fd359f603
Sammy Akharaz <ketrixz@gmail.com>
2018-04-28 21:14:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = {Deadmau5: "Strobe"}
 
 function updatePlaylist(object, artist, title) {
-  return Object.assign({}, {[artist]: title})
+  return Object.assign({}, object, {[artist]: title})
 }
+
+function removePlaylist(object,  )
